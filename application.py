8ce3a08de0628c8f118c33ddac1d4ae52b8a2454
codeAlfred <munozrodas.luisalfredo@gmail.com>
2020-04-22 22:01:29 -0500

    agregando una nueva ruta

--- application.py
+++ application.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return "hola mundo"
 
+@app.route('/prueba')
+def prueba():
+    return "hola prueba como estas"
+
+
 if __name__ == '__main__':
   app.run(port=80, debug=True)
