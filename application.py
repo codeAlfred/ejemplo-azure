2e335fdd4c3de2910e2091589c40d38b9b2238b9
codeAlfred <munozrodas.luisalfredo@gmail.com>
2020-04-22 22:07:06 -0500

    agregando el archivo prueba.html

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/prueba')
 def prueba():
-    return "hola prueba como estas"
+    return render_template("prueba.html")
 
 
 if __name__ == '__main__':
